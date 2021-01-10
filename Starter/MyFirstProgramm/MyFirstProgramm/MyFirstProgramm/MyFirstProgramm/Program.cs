1718d868200f12c70f7cfbda992be715cd434644
SerhiiTuchak <serhiituchak@icloud.com>
2021-01-10 15:04:54 +0200

    First Commit

---
+++ Starter/MyFirstProgramm/MyFirstProgramm/MyFirstProgramm/MyFirstProgramm/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MyFirstProgramm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Starter/MyFirstProgramm/MyFirstProgramm/MyFirstProgramm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstProgramm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
